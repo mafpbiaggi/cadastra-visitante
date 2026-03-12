ed6712556ab44421038facd283913fb74067bc14
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-03-11 23:53:00 -0300

    Update: Invalida token após algum tempo

--- src/Security/CsrfToken.php
+++ src/Security/CsrfToken.php
@@ -2,9 +2,11 @@
 
 namespace App\Security;
 
-class CsrfToken
-{
+class CsrfToken {
+    
     private const SESSION_KEY = 'csrf_token';
+    private const SESSION_TIME = 'csrf_token_time';
+    private const EXPIRY = 300;
 
     public static function start(): void
     {
@@ -19,6 +21,7 @@ class CsrfToken
 
         if (empty($_SESSION[self::SESSION_KEY])) {
             $_SESSION[self::SESSION_KEY] = bin2hex(random_bytes(32));
+            $_SESSION[self::SESSION_TIME] = time();
         }
 
         return $_SESSION[self::SESSION_KEY];
@@ -29,8 +32,14 @@ class CsrfToken
         self::start();
 
         $sessionToken = $_SESSION[self::SESSION_KEY] ?? '';
+        $sessionTime = $_SESSION[self::SESSION_TIME] ?? 0;
         
         if (empty($sessionToken) || empty($token)) {
+            return false;
+        }
+
+        if ((time() - $sessionTime) > self::EXPIRY) {
+            self::invalidate();
             return false;
         }
 
@@ -40,6 +49,7 @@ class CsrfToken
     public static function invalidate(): void
     {
         unset($_SESSION[self::SESSION_KEY]);
+        unset($_SESSION[self::SESSION_TIME]);
     }
 
     public static function getHiddenField(): string
