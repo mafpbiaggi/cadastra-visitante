77e7418ad0c857d2095e27354a8c9df613a94315
Marco Aurélio Biaggi <mafpbiaggi@gmail.com>
2026-03-11 22:47:07 -0300

    Update: Melhora definição e retorno de métodos do model Visitante

--- src/Models/VisitanteModel.php
+++ src/Models/VisitanteModel.php
@@ -7,15 +7,20 @@ use PDOException;
 
 class VisitanteModel {
 
+    private $conn;
+
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
+    }
+
     public function addVisitante(array $data)
     {
         try{
-            $db = new Database();
-            $conn = $db->getInstance();
 
-            $stmt = $conn->prepare("INSERT INTO visitantes (dataVisita, nome, idade, email, telefone, frequentaIgreja,
-                                    pedidoOracao, origem, user_id, church_id, created, modified) VALUES (:dataVisita, :nome, :idade, :email,
-                                    :telefone, :frequentaIgreja, :pedidoOracao, :origem, 22, 66, NOW(), NOW();");
+            $stmt = $this->conn->prepare("INSERT INTO visitantes (dataVisita, nome, idade, email, telefone, frequentaIgreja,
+                                    pedidoOracao, origem, outroComplemento, user_id, church_id, created, modified) VALUES (:dataVisita, :nome, :idade, :email,
+                                    :telefone, :frequentaIgreja, :pedidoOracao, :origem, :outroComplemento, 66, 22, NOW(), NOW())");
             
             $stmt->bindValue(':dataVisita', $data['dataVisita']);
             $stmt->bindValue(':nome', $data['nome']);
@@ -25,14 +30,15 @@ class VisitanteModel {
             $stmt->bindValue(':frequentaIgreja', $data['frequentaIgreja']);
             $stmt->bindValue(':pedidoOracao', $data['pedidoOracao']);
             $stmt->bindValue(':origem', $data['origem']);
+            $stmt->bindValue(':outroComplemento', $data['outroComplemento']);
             $stmt->execute();
 
-            return ['status' => 'true', 'msg' => 'Envio de dados efetuado com sucesso.'];
+            return true;
      
         } catch (PDOException $e) {
             error_log($e->getMessage());
 
-            return ['status' => 'false', 'msg' => 'Envio de dados não efetuado.\nContate o administrador.'];
+            return false;
         }
     }
 }
